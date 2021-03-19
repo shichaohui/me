306269504c5b5935c464c3d4e61f01238e3dcf25
shichaohui <464206200@qq.com>
2021-03-19 17:13:20 +0800

    Refactor

--- src/pages/game/components/Game.vue
+++ src/pages/game/components/Game.vue
@@ -12,12 +12,17 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 
 export default defineComponent({
-  props: ['game'],
+  props: {
+    game: {
+      type: Object as PropType<GameTypes.Game>,
+      required: true
+    }
+  },
   setup(props) {
-    const game = props.game as GameTypes.Game
+    const { game } = props
     return {
       ...game
     }
