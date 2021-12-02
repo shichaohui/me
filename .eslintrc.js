0781c849c8e29861e9f6417ad851752ab40801e1
石朝辉 <shichaohui@wakedata.com>
2021-12-02 17:46:25 +0800

    refactor: update project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
+    'vue/no-unused-vars': 'off'
   },
   overrides: [
     {
